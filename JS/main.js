75d52b01bed71c96c84f9aaaf3906f9763b8fb04
userjohn <johnchacpi479@gmail.com>
2022-10-01 00:03:26 -0500

    started with the logic to create an element for every product

--- JS/main.js
+++ JS/main.js
@@ -40,4 +40,47 @@ function toggleCartAside(){
     aside.classList.toggle("slide-left")
 }
 
+//for the product containers
+const productList = [];// simulates the array that JS would return after consulting the API REST
+productList.push({
+    name: "bike",
+    price: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+}
+)
+productList.push({
+    name: "shoes",
+    price: 40,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+}
+)
+productList.push({
+    name: "Monitor",
+    price: 220,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+}
+)
+{/* <div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" class="product">
+<div class="product-info">
+    <div>
+        <p>120.00</p>
+        <p>Bike</p>
+    </div>
+    <figure>
+        <img src="./Assets/Platzi Yard Sale/Icons/bt_add_to_cart.svg">
+    </figure>
+</div>
+</div> */}
+for (item of productList){
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card")
 
+    const img = document.createElement("img");
+    img.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info")
+
+    const info = document.createElement("div");
+}
