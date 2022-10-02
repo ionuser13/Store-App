75ddfc273c94c1f1d764f62b9bf00cb017071db3
userjohn <johnchacpi479@gmail.com>
2022-10-01 23:44:15 -0500

    improved scroll when asides display, changed padding and margins so the asides look better

--- JS/main.js
+++ JS/main.js
@@ -6,24 +6,30 @@ const burgerBtn = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu")
 //shopping cart aside vars
 const menuCartIcon = document.querySelector(".navbar-shopping-cart")
-const aside = document.querySelector(".product-detail")
-const html = document.querySelector("html")
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer")
+const body = document.querySelector("body")
 //events
 //to open desktop menu and close the cart menu if this is open
 menuEmail.addEventListener("click", toggleMenu);
 function toggleMenu(){
-    const isAsideClosed = aside.classList.contains("slide-left")
+    const isAsideClosed = shoppingCartContainer.classList.contains("slide-left")
     if(!isAsideClosed){
-        aside.classList.add("slide-left")
+        shoppingCartContainer.classList.add("slide-left")
     }
     desktopMenu.classList.toggle("inactive")
 }
 //to open mobile menu and close cart menu if this is open
 burgerBtn.addEventListener("click", toggleMobile)
 function toggleMobile() {
-    const isAsideClosed = aside.classList.contains("slide-left")
+    const isAsideClosed = shoppingCartContainer.classList.contains("slide-left")
     if(!isAsideClosed){
-        aside.classList.add("slide-left")
+        shoppingCartContainer.classList.add("slide-left")
+    }
+    if(!mobileMenu.classList.contains("slide-right")){
+        body.classList.remove("height")
+    }
+    else{
+        body.classList.add("height")
     }
     mobileMenu.classList.toggle("slide-right")
 }
@@ -38,7 +44,13 @@ function toggleCartAside(){
     if(!isMobileMenuClosed){
         mobileMenu.classList.add("slide-right")
     }
-    aside.classList.toggle("slide-left")
+    if(!shoppingCartContainer.classList.contains("slide-left")){
+        body.classList.remove("height")
+    }
+    else{
+        body.classList.add("height")
+    }
+    shoppingCartContainer.classList.toggle("slide-left")
 }
 
 //for the product containers
