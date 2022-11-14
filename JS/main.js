dbcd75d67aadfc563e00398bebba611ce537069c
userjohn <johnchacpi479@gmail.com>
2022-11-14 17:19:10 -0500

    made add cart button and checkout button unreacheable with keyboard

--- JS/main.js
+++ JS/main.js
@@ -13,6 +13,7 @@ const productDetailContainer = document.querySelector("#productDetail");
 const productDetailClose = document.querySelector(".product-detail-close");
 //close cart
 const closeCart = document.querySelector(".close-cart");
+
 //events
 //to open desktop menu and close the cart menu if this is open
 menuEmail.addEventListener("click", toggleMenu);
@@ -46,24 +47,28 @@ menuCartIcon.addEventListener("click", toggleCartAside)
 function toggleCartAside(){
     const isMobileMenuClosed = mobileMenu.classList.contains("slide-right");
     const isDesktopEmailClosed = desktopMenu.classList.contains("inactive");
+
     if(!isDesktopEmailClosed){
         desktopMenu.classList.add("inactive");
     }
+
     if(!isMobileMenuClosed){
         mobileMenu.classList.add("slide-right")
     }
+
     if(!shoppingCartContainer.classList.contains("slide-left")){
         body.classList.remove("height")
     }
     else{
         body.classList.add("height")
     }
+    
     const isProductDetailClosed = productDetailContainer.classList.contains("slide-left");
     if(!isProductDetailClosed){
         productDetailContainer.classList.add("slide-left");
     }
-    shoppingCartContainer.classList.toggle("slide-left")
-    shoppingCartContainer.classList.toggle("height2")
+    shoppingCartContainer.classList.toggle("slide-left");
+    shoppingCartContainer.classList.toggle("height2");
 }
 //opens the aside when clicking an image from the store
 function openProductDetail() {
@@ -84,6 +89,7 @@ closeCart.addEventListener("click", closeShopCart)
 function closeShopCart(){
     shoppingCartContainer.classList.add("slide-left");
 }
+
 //for the product containers
 const productList = [];// simulates the array that JS would return after consulting the API REST
 productList.push({
@@ -169,34 +175,36 @@ productList.push({
     price: 220,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 }
-)   
+)
+
 function renderImage(arr){
     const cardsContainer = document.querySelector(".cards-container")
-for (item of productList){
-    const productCard = document.createElement("div");
-    productCard.classList.add("product-card")
+    for (item of productList){
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card")
 
-    const img = document.createElement("img");
-    img.setAttribute("src", item.image);
-    img.addEventListener("click", openProductDetail)
+        const img = document.createElement("img");
+        img.setAttribute("src", item.image);
+        img.addEventListener("click", openProductDetail)
 
-    const productInfo = document.createElement("div");
-    productInfo.classList.add("product-info")
-    
-    const info = document.createElement("div");
+        const productInfo = document.createElement("div");
+        productInfo.classList.add("product-info")
+        
+        const info = document.createElement("div");
 
-    const productPrice = document.createElement("p");
-    productPrice.innerText = "$" + item.price;
+        const productPrice = document.createElement("p");
+        productPrice.innerText = "$" + item.price;
 
-    const productName = document.createElement("p");
-    productName.innerText = item.name;
+        const productName = document.createElement("p");
+        productName.innerText = item.name;
 
-    const productInfoFigure = document.createElement("figure");
-    
-    const productImgCart = document.createElement("img")
-    productImgCart.setAttribute("src", "./Assets/Platzi Yard Sale/Icons/bt_add_to_cart.svg");
-    productImgCart.classList.add("cart-image")
+        const productInfoFigure = document.createElement("figure");
+        
+        const productImgCart = document.createElement("img")
+        productImgCart.setAttribute("src", "./Assets/Platzi Yard Sale/Icons/bt_add_to_cart.svg");
+        productImgCart.classList.add("cart-image")
 //we've created created all the elements
+
 //declares which element goes inside another
     productInfoFigure.appendChild(productImgCart);
 
