64a04da52f31f21aec71126b30386951ac6db657
userjohn <johnchacpi479@gmail.com>
2022-11-15 11:28:49 -0500

    added modules

---
+++ JS/main-selectors.js
@@ -0,0 +1,19 @@
+export const menuEmail = document.querySelector(".navbar-email");//menu desktop
+
+export const desktopMenu = document.querySelector(".desktop-menu");//mobile menu vars
+
+export const burgerBtn = document.querySelector(".menu");
+export const mobileMenu = document.querySelector(".mobile-menu");
+
+//shopping cart aside vars
+export const menuCartIcon = document.querySelector(".navbar-shopping-cart")
+export const shoppingCartContainer = document.querySelector("#shoppingCartContainer")
+export const body = document.querySelector("body");
+
+//product detail selector
+export const productDetailContainer = document.querySelector("#productDetail");
+export const productDetailClose = document.querySelector(".product-detail-close");
+
+//close cart
+export const closeCart = document.querySelector(".close-cart");
+export const cardsContainer = document.querySelector(".cards-container")
---
+++ JS/navbar-selectors.js
@@ -0,0 +1,7 @@
+export * from "./navbar-selectors.js"
+const all = document.querySelector(".all");
+const clothes = document.querySelector(".clothes");
+const electronics = document.querySelector(".electronics");
+const furnitures = document.querySelector(".furnitures");
+const toys = document.querySelector(".toys");
+const others = document.querySelector(".others");
