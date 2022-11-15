5ea1516be2b8ce0689d944aa2b4c8830b3f1220a
userjohn <johnchacpi479@gmail.com>
2022-11-15 16:08:00 -0500

    added api functionality to mobile menu options

--- JS/navbar-selectors.js
+++ JS/navbar-selectors.js
@@ -1,38 +1,57 @@
-export const all = document.querySelector(".all");
-export const clothes = document.querySelector(".clothes");
-export const electronics = document.querySelector(".electronics");
-export const furnitures = document.querySelector(".furnitures");
-export const toys = document.querySelector(".toys");
-export const others = document.querySelector(".others");
+export const all = document.querySelectorAll(".all");
+export const clothes = document.querySelectorAll(".clothes");
+export const electronics = document.querySelectorAll(".electronics");
+export const furnitures = document.querySelectorAll(".furnitures");
+export const toys = document.querySelectorAll(".toys");
+export const others = document.querySelectorAll(".others");
 
 import {loadProducts} from "./main.js";
 import { api } from "./main.js"
 import { cardsContainer } from "./main-selectors.js";
+import {mobileMenu} from "./main-selectors.js"
 
-all.addEventListener("click", () => {
-    cardsContainer.innerHTML = "";
-    loadProducts(`${api}products?offset=0&limit=10`)})
+all.forEach(btn => {
+    btn.addEventListener("click", () => {
+        mobileMenu.classList.add("slide-right");
+        cardsContainer.innerText = "";
+        loadProducts(`${api}products?offset=0&limit=10`)}
+)})
 
-clothes.addEventListener("click", () => {
-    cardsContainer.innerHTML = "";
-    loadProducts(`${api}categories/1/products`)
-});
-
-electronics.addEventListener("click", () => {
-    cardsContainer.innerHTML = "";
-    loadProducts(`${api}categories/2/products`)
+clothes.forEach(btn => {
+    btn.addEventListener("click", () => {
+        cardsContainer.innerText = "";
+        mobileMenu.classList.add("slide-right")
+        loadProducts(`${api}categories/1/products`);
+    });
 })
 
-furnitures.addEventListener("click", () => {
-    cardsContainer.innerHTML = "";
-    loadProducts(`${api}categories/3/products`)
+electronics.forEach(btn => {
+    btn.addEventListener("click", () => {
+        mobileMenu.classList.add("slide-right")
+        cardsContainer.innerText = "";
+        loadProducts(`${api}categories/2/products`)
+    })
 })
 
-toys.addEventListener("click", () => {
-    cardsContainer.innerHTML = "";
-    loadProducts(`${api}categories/4/products`)
+furnitures.forEach(btn => {
+    btn.addEventListener("click", () => {
+        mobileMenu.classList.add("slide-right")
+        cardsContainer.innerText = "";
+        loadProducts(`${api}categories/3/products`)
+    })
 })
-others.addEventListener("click", () => {
-    cardsContainer.innerHTML = "";
-    loadProducts(`${api}categories/5/products`)
+
+toys.forEach(btn => {
+    btn.addEventListener("click", () => {
+        mobileMenu.classList.add("slide-right")
+        cardsContainer.innerText = "";
+        loadProducts(`${api}categories/4/products`)
+    })
+})
+others.forEach(btn => {
+    btn.addEventListener("click", () => {
+        mobileMenu.classList.add("slide-right")
+        cardsContainer.innerText = "";
+        loadProducts(`${api}categories/5/products`)
+    })
 })
